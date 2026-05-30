4726126b3025d2f5a4d443a901fb7b4c4ea284c7
Агаджанян Тигран Ваграмович <tvagadzhanian@edu.hse.ru>
2026-05-30 14:08:59 +0300

    add: first version

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,106 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import FormGenerator from './components/FormGenerator.vue'
+
+const formData = ref<Record<string, any>>({})
+const formSchema = ref({
+	"fields": [
+		{ "type": "text", "label": "Имя", "model": "name", "required": true },
+		{ "type": "email", "label": "Email", "model": "email", "required": true },
+		{ "type": "password", "label": "Пароль", "model": "password", "required": true, "minLength": 6 },
+		{ "type": "select", "label": "Роль", "model": "role", "options": ["Админ", "Пользователь"], "required": true },
+		{ "type": "checkbox", "label": "Согласен с условиями", "model": "terms", "required": true }
+	]
+})
+const formError = ref(false)
+const schemaString = computed({
+	get: () => JSON.stringify(formSchema.value, null, 2),
+	set: (val) => {
+		// try {
+		// 	formSchema.value = JSON.parse(val)
+		// } catch (e) {
+		// 	formError.value = !formError.value
+		// }
+		formSchema.value = JSON.parse(val)
+	}
+})
+const formDataString = computed(() => JSON.stringify(formData.value, null, 2))
 </script>
 
 <template>
 	<div class="first-screen">
 		<div class="first-screen__container">
-			<h1 class="first-screen__title">Генератор HTML формы</h1>
+			<h1 class="first-screen__title">Генератор HTML форм</h1>
+			<div class="first-screen__body">
+				<div class="first-screen__column">
+					<div class="first-screen__field">
+						<label for="upload-json-textarea" class="first-screen__label">Вставьте JSON-схему</label>
+						<textarea name="upload-json-textarea" id="upload-json-textarea" v-model="schemaString"
+							class="first-screen__textarea" :class="{'first-screen__textarea--error': formError}" ></textarea>
+					</div>
+					<div class="first-screen__field">
+						<label for="reactive-form-data-textarea" class="first-screen__label">Состояние формы</label>
+						<textarea name="reactive-form-data-textarea" id="reactive-form-data-textarea" readonly v-model="formDataString"
+							class="first-screen__textarea"></textarea>
+					</div>
+				</div>
+				<div class="first-screen__column">
+					<div v-if="true" class="first-screen__column-title">Ваша форма</div>
+					<FormGenerator :schema="formSchema" v-model="formData" />
+				</div>
+			</div>
 		</div>
 	</div>
-	<!-- <FormGenerator :schema="formSchema" v-model="formData" /> -->
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.first-screen {
+	padding: toRem(50) 0;
+
+	&__container {}
+
+	&__title {
+		font-weight: 600;
+		font-size: toRem(40);
+		text-align: center;
+		margin-bottom: toRem(48);
+	}
+
+	&__body {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: toRem(40);
+	}
+
+	&__column {}
+
+	&__column-title {
+		font-size: toRem(20);
+		margin-bottom: toRem(16);
+	}
+
+	&__field {
+		&:not(:last-child) {
+			margin-bottom: toRem(32);
+		}
+	}
+
+	&__label {
+		font-size: toRem(20);
+		margin-bottom: toRem(16);
+	}
+
+	&__textarea {
+		height: toRem(250);
+		border-radius: 8px;
+		padding: toRem(16);
+		resize: none;
+		background-color: #fff;
+		border: 2px solid $mainColor;
+		&--error{
+			border-color: red;
+			outline-color: red;
+		}
+	}
+}
+</style>
--- src/components/FormGenerator.vue
+++ src/components/FormGenerator.vue
@@ -1,6 +1,161 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface FormField {
+	type: 'text' | 'email' | 'password' | 'select' | 'checkbox'
+	label: string
+	model: string
+	required?: boolean
+	minLength?: number
+	pattern?: string
+	options?: string[]
+}
+interface FormSchema {
+	fields: FormField[]
+}
+
+const props = defineProps<{
+	schema: FormSchema
+}>()
+const formData = defineModel<Record<string, any>>({ default: () => ({}) })
+const errors = ref<Record<string, string>>({})
+
+const handleSubmit = (event: Event) => {
+	errors.value = {}
+	let isValid = true
+
+	props.schema.fields.forEach((field) => {
+		const value = formData.value[field.model]
+
+		// Проверка на Required
+		if (field.required && (value === undefined || value === null || value === '' || value === false)) {
+			errors.value[field.model] = `Поле "${field.label}" обязательно для заполнения`
+			isValid = false
+			return
+		}
+		// Проверка на MinLength
+		if (field.minLength && typeof value === 'string' && value.length < field.minLength) {
+			errors.value[field.model] = `Минимальная длина – ${field.minLength} символов`
+			isValid = false
+			return
+		}
+		// Проверка на Pattern (RegExp)
+		if (field.pattern && typeof value === 'string') {
+			const regex = new RegExp(field.pattern)
+			if (!regex.test(value)) {
+				errors.value[field.model] = `Поле не соответствует формату`
+				isValid = false
+			}
+		}
+	})
+
+	if (isValid) {
+		alert('Форма валидна!')
+	}
+}
+</script>
 
 <template>
+	<form @submit.prevent="handleSubmit" class="form-generator">
+		<div v-for="field in props.schema?.fields" :key="field.model" class="form-generator__field">
+			<label :for="field.model" class="form-generator__label">
+				{{ field.label }} <span v-if="field.required" class="form-generator__label-required-star">*</span>
+			</label>
+
+			<template v-if="field.type === 'select'">
+				<select :id="field.model" v-model="formData[field.model]" class="form-generator__input">
+					<option value="" disabled selected>Выберите значение</option>
+					<option v-for="opt in field.options" :key="opt" :value="opt">
+						{{ opt }}
+					</option>
+				</select>
+			</template>
+
+			<template v-else-if="field.type === 'checkbox'">
+				<div class="form-generator__checkbox">
+					<input type="checkbox" :id="field.model" v-model="formData[field.model]" />
+					<label :for="field.model" class="form-generator__checkbox-text">Да</label>
+				</div>
+			</template>
+
+			<template v-else>
+				<input :type="field.type" :id="field.model" v-model="formData[field.model]" class="form-generator__input"
+					:placeholder="`Введите ${field.label.toLowerCase()}`" />
+			</template>
+
+			<span v-if="errors[field.model]" class="form-generator__error">
+				{{ errors[field.model] }}
+			</span>
+		</div>
+
+		<button type="submit" class="form-generator__submit-btn">Проверить на валидность</button>
+	</form>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.form-generator {
+	display: flex;
+	flex-direction: column;
+	gap: toRem(24);
+	padding: toRem(16);
+	border: 1px solid $mainColor;
+	border-radius: 8px;
+	background: #fff;
+
+	&__label-required-star {
+		color: red;
+	}
+
+	&__field {
+		display: flex;
+		flex-direction: column;
+		gap: toRem(8);
+	}
+
+	&__input {
+		border-radius: 8px;
+		padding: toRem(8) toRem(16);
+		border: 1px solid $mainColor;
+	}
+
+	&__error {
+		color: red;
+		font-size: toRem(12);
+		margin-top: toRem(4);
+	}
+
+	&__submit-btn {
+		padding: toRem(8) toRem(24);
+		background: $orangeColor;
+		transition: all 0.3s;
+		color: white;
+		width: 100%;
+		border: none;
+		font-weight: 600;
+		border-radius: toRem(16);
+		cursor: pointer;
+
+		&:hover {
+			background: $orangeHoverColor;
+		}
+	}
+
+	&__checkbox {
+		display: flex;
+		align-items: center;
+		justify-content: end;
+
+		label,
+		input {
+			cursor: pointer;
+		}
+
+		input {
+			width: auto;
+			margin-right: toRem(8);
+		}
+	}
+
+	&__checkbox-text {}
+}
+</style>
